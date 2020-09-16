468bc75b6cffe1266bdc3841ba373da52e74b500
john <jgh2102@gmail.com>
2020-09-17 06:00:51 +0900

    finished 05

--- data-access-layer/05-insert-new-instruction.sql
+++ data-access-layer/05-insert-new-instruction.sql
@@ -42,4 +42,14 @@
 -- )
 
 
--- YOUR CODE HERE
+INSERT INTO instructions (list_order, specification, recipe_id)
+VALUES(
+    (SELECT COALESCE(MAX(list_order)+1, null, 1) FROM instructions 
+        WHERE recipe_id = $2), $1, $2);
+
+-- INSERT INTO instructions (list_order, specification, recipe_id)
+-- VALUES(
+--     (SELECT COALESCE(NULL, 1) FROM instructions 
+--         WHERE recipe_id = $2), (MAX(list_order))+1, $2
+
+-- );
