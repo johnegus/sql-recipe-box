47852b2438597f131306cd215068c0c39f8320ff
Michael Shuff <mjshuff23@gmail.com>
2020-09-16 13:24:40 -0400

    finished 00a-schema.sql

--- data-access-layer/00a-schema.sql
+++ data-access-layer/00a-schema.sql
@@ -8,13 +8,11 @@ DROP USER IF EXISTS recipe_box_app;
 -- 'recipe_box_app'. It will need a password set to 'SPuaQ3no'. Best not to make
 -- it a super user.
 
--- YOUR CODE HERE
-
-
-
+CREATE USER recipe_box_app WITH PASSWORD 'sPuaQ3no';
 
 -- You will need to create a new database for the application named
 -- 'recipe_box'. It should be owned by 'recipe_box_app'. You may want to revoke
 -- connect privileges from PUBLIC just to have good security.
 
--- YOUR CODE HERE
+CREATE DATABASE recipe_box WITH OWNER recipe_box_app;
+REVOKE CONNECTION ON DATABASE recipe_box FROM PUBLIC;
